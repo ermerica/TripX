3e873f814db0f68c1b74d573494ed903f0a953b6
ermerica <erichardson9@cfl.rr.com>
2015-11-05 08:26:10 -0500

    DB001
    DB001

---
+++ functions.php
@@ -0,0 +1,41 @@
+<?php
+	
+	//error_reporting(E_ALL);
+	//ini_set('display_errors', 1);
+	
+	require_once('config/data.php');
+	
+	//load template part
+	if(!function_exists('load_template')) {
+		function load_template($template) {
+			
+			$result = false;
+			$template = $template.'.php';
+			
+			if(file_exists($template)) {
+				include_once($template);
+				$result = true;
+			}
+			else {
+				include_once('home.php');
+			}
+			
+			return $result;
+			
+		}
+	}
+
+	
+	if(!function_exists('base_url')) {
+		function base_url() {
+			if($_SERVER['HTTPS']) {
+				$prefix = 'https';
+			}
+			else {
+				$prefix = 'http';
+			}
+			return $prefix.'://'.BASE_URL;
+		}
+	}
+	
+?>
---
+++ header.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+  <head>
+		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Trip Estimator</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    
+    <!-- Main Styles -->
+    <link rel="stylesheet" href="<?php echo base_url().'/css/style.css'; ?>">
+    
+    <!-- Latest jquery -->
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    
+    
+    <script type="text/javascript">
+      jQuery(function() {
+        console.log('testing');
+      });
+    </script>
+    
+  </head>
+  
+  <body>
+  	
+    <div class="container">
+    
+    	<div class="header row">
+      	<div class="header-content col-xs-12">
+        	<img class="logo" src="<?php echo base_url(); ?>/images/logo.jpg" />
+        </div>
+      </div>
+      
+      <div class="main row">
+      	<div class="main-content col-xs-12">
